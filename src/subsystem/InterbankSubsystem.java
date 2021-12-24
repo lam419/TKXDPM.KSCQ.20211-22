50cf6122b3f080e7db59db719071f84829f478ad
luongbs01 <luongbs01@gmail.com>
2021-12-24 09:46:52 +0700

    package subsystem, subsystem.interbank

--- src/subsystem/InterbankSubsystem.java
+++ src/subsystem/InterbankSubsystem.java
@@ -4,21 +4,40 @@ import common.exception.PaymentException;
 import common.exception.UnrecognizedException;
 import entity.payment.CreditCard;
 import entity.payment.PaymentTransaction;
+import subsystem.interbank.InterbankSubsystemController;
 
 public class InterbankSubsystem implements InterbankInterface {
 
-	@Override
-	public PaymentTransaction payOrder(CreditCard card, int amount, String contents)
-			throws PaymentException, UnrecognizedException {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Represent the controller of the subsystem
+	 */
+	private InterbankSubsystemController ctrl;
+
+	/**
+	 * Initializes a newly created {@code InterbankSubsystem} object so that it
+	 * represents an Interbank subsystem.
+	 */
+	public InterbankSubsystem() {
+		String str = new String();
+		this.ctrl = new InterbankSubsystemController();
 	}
 
-	@Override
-	public PaymentTransaction refund(CreditCard card, int amount, String contents)
-			throws PaymentException, UnrecognizedException {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * @see InterbankInterface#payOrder(entity.payment.CreditCard, int,
+	 *      java.lang.String)
+	 */
+	public PaymentTransaction payOrder(CreditCard card, int amount, String contents) {
+		PaymentTransaction transaction = ctrl.payOrder(card, amount, contents);
+		return transaction;
+	}
+
+	/**
+	 * @see InterbankInterface#refund(entity.payment.CreditCard, int,
+	 *      java.lang.String)
+	 */
+	public PaymentTransaction refund(CreditCard card, int amount, String contents) {
+		PaymentTransaction transaction = ctrl.refund(card, amount, contents);
+		return transaction;
 	}
 
 }
