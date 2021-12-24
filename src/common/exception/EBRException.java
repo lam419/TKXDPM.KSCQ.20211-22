88de58f4e1bd0d46dbb9abee4c1f0674b9cd1b48
luongbs01 <luongbs01@gmail.com>
2021-12-24 09:05:17 +0700

    exception

---
+++ src/common/exception/EBRException.java
@@ -0,0 +1,13 @@
+package common.exception;
+
+public class EBRException extends RuntimeException {
+
+	public EBRException() {
+
+	}
+
+	public EBRException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/common/exception/InvalidCardException.java
@@ -0,0 +1,9 @@
+package common.exception;
+
+public class InvalidCardException extends PaymentException {
+	
+	public InvalidCardException() {
+		super("ERROR: Invalid card!");
+	}
+	
+}
