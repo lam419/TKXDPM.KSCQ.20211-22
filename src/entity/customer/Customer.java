17e426b93f5ebaff0b188b1eee45ea20c15e1f55
luongbs01 <luongbs01@gmail.com>
2021-12-27 08:51:08 +0700

    add entity: bike, station, order

--- src/entity/customer/Customer.java
+++ src/entity/customer/Customer.java
@@ -1,6 +1,6 @@
-package entity.user;
+package entity.customer;
 
-public class User {
+public class Customer {
 
 	private int id;
 	private String name;
@@ -8,7 +8,7 @@ public class User {
 	private String address;
 	private String phone;
 
-	public User(int id, String name, String email, String address, String phone) {
+	public Customer(int id, String name, String email, String address, String phone) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -21,6 +21,14 @@ public class User {
 	public String toString() {
 		return "{" + "  username='" + name + "'" + ", email='" + email + "'" + ", address='" + address + "'"
 				+ ", phone='" + phone + "'" + "}";
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	// getter and setter
